571ab0ba8cb01ddd7bf1e419cd93c751bea571c9
kiannaleah <kianna.ak12@gmail.com>
2019-02-12 11:33:27 -0700

    updates on songwriting

--- project1/songwriting.js
+++ project1/songwriting.js
@@ -7,46 +7,68 @@
 4. Display results for each prompt selected 
 */
 
-var words_prompts = [
+let songPrompts = {
+
+word: [
 "timeless", "quiet", "did you mean it?", "I hope you did", "talk", "long day", 
 "forget", "king", "not anymore", "paint", "the color pink", "all day", "I hear you", 
 "said", "awake", "shoes", "restless", "mirrior", "dandelions", "flawed", "blur",
 "present", "before & after", "eyes", "insignificant"
-];
+],
 
-var mood = [ "tired", "upbeat", "summer vibes", "tense", "anxious", "hopeful",
+mood: [ "tired", "upbeat", "summer vibes", "tense", "anxious", "hopeful",
 "heartbroken", "in love", "fragile", "romantic", "isolated", "nostalgic", "devoted",
 "chill", "bright/colorful", "triumph", "dramatic", "introspective", "motivating", "yearning",
 "serene"
-];
+],
 
-var scene = [ "moving", "montage", "growing up", "an ending", "a beginning", 
+scene: [ "moving", "montage", "growing up", "an ending", "a beginning", 
 "first kiss", "roadtrip", "morning", "walking in paris", "breakup", 
 "wedding scene", "opening scene", "someone talking to a gravestone", 
 "flashback to childhood", "an accusation", "someone brought to tears",
 "two people talking while dancing", "meeting someone new"
-];
+],
 
-var key_sig = [ "C Major", "G Major", "D Major","A Major","E Major","B Major", "F Major",
+key: [ "C Major", "G Major", "D Major","A Major","E Major","B Major", "F Major",
 "E Minor", "B Minor", "F# Minor", "C# Minor", "G# Minor", "D# Minor", "A# Minor", 
 "D Minor", "G Minor", "C Minor", "F Minor", "Bb Minor", "Eb Minor",
 "Ab Minor"
-];
+],
 
-var time_sig = [ "4/4", "3/4", "2/4", "6/8", "2/2"];
+time: [ "4/4", "3/4", "2/4", "6/8", "2/2"],
 
-var bpm = [ "80-115", "90-110", "120", "120-160", "140", "66-76", 
+bpm: [ "80-115", "90-110", "120", "120-160", "140", "66-76", 
 "76-108", "60-66"
-];
+],
+}
+
+//reference point for array of checkboxes
+let prompts = document.forms['songForm'].elements['prompt[]'];
 
 function checkedStatus() {
-    //loop through all the check boxes and see which ones are checked
     
-
+    let specificArray = [];
+    
+    //loop through all the check boxes and see which ones are checked
+    for (i=0; i < prompts.length; i++) {
+        if (prompts[i].checked) {
+        //if checked find matching array from songPrompts object
+         specificArray = songPrompts[prompts[i].value];
+           //send specific songPrompt array into random prompt generator 
+    selectRandom(specificArray);
+        } 
+}
 }
 
 
-function random(){
-    Math.floor(Math.random * Array.length)
+function selectRandom(promptArray){
+   //get a random prompt from the array
+   const option = Math.floor(Math.random() * promptArray.length);
+   //display random array element to user
+   let item = document.createElement("li");
+   item.innerHTML = promptArray[option];
+   document.getElementById("display").appendChild(item);
 }
 
+
+    
